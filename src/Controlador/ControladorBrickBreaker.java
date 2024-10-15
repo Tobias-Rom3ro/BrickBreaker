aa35a5064769f5d14faebc272c9daa4f5d7941e6
Tatys2718 <mora37597@gmail.com>
2024-10-15 08:31:03 -0500

    Estoy intentando hacer lo del historial

--- src/Controlador/ControladorBrickBreaker.java
+++ src/Controlador/ControladorBrickBreaker.java
@@ -174,15 +174,7 @@ public class ControladorBrickBreaker implements KeyListener, ActionListener {
                             }
 
                             // Si todos los ladrillos han sido destruidos, avanzar al siguiente nivel
-                            if (!modelo.isPlay()) {
-                                if (modelo.getTotalBricks() <= 0) {
-                                    if (modelo.getLevelManager().hayMasNiveles()) {
-                                        sonidoVictoria.reproducir();
-                                    } else {
-                                        sonidoVictoria.reproducir();
-                                    }
-                                }
-                            }
+
                             if (modelo.getTotalBricks() <= 0) {
                                 sonidoVictoria.reproducir();
                                 modelo.avanzarNivel();
